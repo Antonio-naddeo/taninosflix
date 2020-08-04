a0a3e168e174411320e9e8396d0dfab9e2539ae7
Antonio de Barros <antonio.naddeomf@gmail.com>
2020-08-04 12:57:13 -0300

    commit redondo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,11 @@ function Footer() {
         Orgulhosamente criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          Imersão React da Alura 
+        </a>
+        &nbsp;por&nbsp;
+        <a href="https://github.com/Antonio-naddeo">
+           Taninos(ou eu mesmo)
         </a>
       </p>
     </FooterBase>
